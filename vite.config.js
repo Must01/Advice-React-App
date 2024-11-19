49cd6640b98371e43df0c6ffd9f8402bffd8438a
Mustapha Bouddahr <mustaphabouddahr347@gmail.com>
2024-11-19 23:11:39 +0100

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure the build outputs to 'dist' (default, but can be explicitly stated)
+    outDir: "dist", // Ensure the output directory is 'dist'
     rollupOptions: {
-      external: [], // External dependencies, if any
+      external: [],
     },
-    // You can also enable minification or other optimizations here, if needed:
-    minify: "esbuild", // or 'terser' if you prefer
-    // If you have static assets like images, you might want to configure assetsDir:
-    assetsDir: "assets", // Optional: defines the folder for static assets inside 'dist'
   },
 });
